72210412b1f52f43de87a9ed24e68ecd5b9591a7
Mohamed Gassama <gassma803@gmail.com>
2026-08-22 12:42:14 +0000

    Avoid mobile menu overlay flash

--- en/privacy.php
+++ en/privacy.php
@@ -9,7 +9,7 @@
     <link rel="icon" href="../assets/images/favicon.png" sizes="32x32">
     <link rel="preload" href="../assets/fonts/Montserrat-Regular.woff2" as="font" type="font/woff2" crossorigin>
     <link rel="preload" href="../assets/fonts/Montserrat-SemiBold.woff2" as="font" type="font/woff2" crossorigin>
-    <link rel="stylesheet" href="../assets/css/styles.css?v=20260822-media-short">
+    <link rel="stylesheet" href="../assets/css/styles.css?v=20260822-menu-flash">
     <link rel="canonical" href="https://www.groupebabia.com/en/privacy.php">
     <link rel="alternate" hreflang="fr" href="https://www.groupebabia.com/confidentialite.php">
     <link rel="alternate" hreflang="en" href="https://www.groupebabia.com/en/privacy.php">
